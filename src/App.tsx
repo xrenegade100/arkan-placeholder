bf9dbd93ec8c89ce29319518e99b9a803d77e3ae
Giovanni <scorziello.giovanni00@gmail.com>
2022-11-25 20:46:49 +0100

    fix: the site is now rwsponsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import Typewriter, { TypewriterClass} from 'typewriter-effect';
-import './App.css';
 import icon from './images/eye.svg';
 
 const App: React.FC = () => (
-    <div className='container'>
-        <img className='eye-image' src={icon}/>
-        <div className='cover' />
-        <Typewriter
-            options={{
-                cursor: '_',
-            }}
+    <div className='w-full h-screen p-0 m-0 absolute'>
+        <div className='-z-10 w-full h-screen flex justify-center items-center animate-cover'>
+            <img src={icon} className="w-72 xl:w-96 h-auto animate-fade-in"/>
+        </div>
+        <div className='absolute top-0 left-0 z-0 w-full h-screen flex justify-center items-center'>
+            <span className='font-logo text-logo text-8xl xl:text-9xl'>
+                <Typewriter
+                    options={{
+                        loop: false,
+                        cursor: '_',
+                    }}
 
-            onInit={(typewriter: TypewriterClass) => {
-                typewriter
-                    .pauseFor(5000)
-                    .changeDelay(400)
-                    .typeString('<span style="color: #fff0;">_</span>ARKAN')
-                    .start();
-            }}/>
+                    onInit={(typewriter: TypewriterClass) => {
+                        typewriter
+                            .pauseFor(2000)
+                            .changeDelay(400)
+                            .typeString('<span style="color: #fff0;">_</span>ARKAN')
+                            .start();
+            }}
+        />
+        </span>
+        </div>
     </div>
 );
 
